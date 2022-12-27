6cce322843cd487de77a4c26bc10f9860c1873e3
Aaron Wallis <wallywheels@gmail.com>
2022-12-27 10:32:27 -0800

    First commit

---
+++ lib/ardy_kafka.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ardy_kafka/version"
+
+module ArdyKafka
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ardy_kafka/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ArdyKafka
+  VERSION = "0.1.0"
+end
