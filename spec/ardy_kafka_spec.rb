d722074ec0cb53856d70a396f4d26bd24f36165a
Aaron Wallis <wallywheels@gmail.com>
2022-12-27 10:49:04 -0800

    base module spec

--- spec/ardy_kafka_spec.rb
+++ spec/ardy_kafka_spec.rb
@@ -5,7 +5,75 @@ RSpec.describe ArdyKafka do
     expect(ArdyKafka::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.configure' do
+    subject(:config) { ArdyKafka.config }
+
+    context 'with default settings' do
+      before do 
+        ArdyKafka.configure { |c| }
+      end
+
+      it 'sets the defaults' do 
+        expect(config.blocking_exceptions).to eq(ArdyKafka::DEFAULTS[:blocking_exceptions])
+        expect(config.non_blocking_exceptions).to eq(ArdyKafka::DEFAULTS[:non_blocking_exceptions])
+        expect(config.shutdown_timeout).to eq(ArdyKafka::DEFAULTS[:shutdown_timeout])
+      end
+    end
+
+    context 'with configurable settings' do
+      let(:brokers) { 'localhost:9092' }
+      let(:shutdown_timeout) { 20 }
+      let(:blocking_exceptions) { [BlockingError] }
+      let(:non_blocking_exceptions) { [NonBlockingError] }
+
+      before do
+        stub_const('::BlockingError', StandardError)
+        stub_const('::NonBlockingError', StandardError)
+
+        ArdyKafka.configure do |c|
+          c.brokers = brokers
+          c.shutdown_timeout = shutdown_timeout
+          c.blocking_exceptions = blocking_exceptions
+          c.non_blocking_exceptions = non_blocking_exceptions
+        end
+      end
+
+      it 'stores the settings', :aggregate_failures do
+        expect(config.brokers).to eq(brokers)
+        expect(config.shutdown_timeout).to eq(shutdown_timeout)
+        expect(config.blocking_exceptions).to eq(blocking_exceptions)
+        expect(config.non_blocking_exceptions).to eq(non_blocking_exceptions)
+      end
+    end
+
+    context 'with unconfigurable settings' do
+      it 'raises an error' do 
+        expect do
+          ArdyKafka.configure do |c|
+            c.foo = 'bar'
+          end
+        end.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  describe '.config' do
+    it 'returns a config object' do
+      expect(described_class.config).to be_a(ArdyKafka::Config)
+    end
+  end
+
+  describe '.base_kafka_config' do
+    before do
+      ArdyKafka.configure do |c|
+        c.brokers = 'localhost:9092'
+      end
+    end
+
+    it do
+      expect(described_class.base_kafka_config).to eq({
+        :"bootstrap.servers" => 'localhost:9092'
+      })
+    end
   end
 end
