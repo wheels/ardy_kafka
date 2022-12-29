95ecb07c55fc3d75fd24506a3180d208b7594329
Aaron Wallis <wallywheels@gmail.com>
2022-12-28 16:01:31 -0800

    Support remaining method args for rdkafka producer

--- lib/ardy_kafka/producer.rb
+++ lib/ardy_kafka/producer.rb
@@ -2,12 +2,17 @@ module ArdyKafka
   class Producer
     attr_reader :driver_producer, :result
 
-    def produce(topic:, payload:)
+    def produce(topic:, payload: nil, key: nil, partition: nil, partition_key: nil, timestamp: nil, headers: nil)
       ArdyKafka.producer_pool.with do |producer|
         @driver_producer = producer
         @result = @driver_producer.produce(
           topic: topic,
-          payload: ArdyKafka.encode_json(payload)
+          payload: ArdyKafka.encode_json(payload),
+          key: key,
+          partition: partition,
+          partition_key: partition_key,
+          timestamp: timestamp,
+          headers: headers
         ).wait
       end
     end
